4f79acbf53b0df6f2a376343738373aa26310e66
lqun <lqun@example.com>
2026-01-11 12:34:19 +0900

    update

--- public/client.js
+++ public/client.js
@@ -4,35 +4,52 @@ const socket = io();
 function join() {
 const name = document.getElementById("name").value;
 socket.emit("join", name);
-document.getElementById("join").hidden = true;
-document.getElementById("game").hidden = false;
 }
 
 
-function sendChat() {
-socket.emit("chat", document.getElementById("chat").value);
+function startGame() {
+socket.emit("startGame");
 }
+
+
+socket.on("errorMsg", msg => alert(msg));
 
 
 socket.on("state", state => {
+document.getElementById("players").innerHTML = state.players
+.map((p, i) => `${i === state.turnIndex ? "👉" : ""}${p.name} HP:${p.hp}`)
+.join("<br>");
+
+
 const me = state.players.find(p => p.id === socket.id);
+const hand = document.getElementById("hand");
+hand.innerHTML = "";
 
 
-players.innerHTML = state.players.map((p, i) =>
-`<div class="player ${i === state.turn ? 'turn' : ''}">
-<span style="color:${i===0?'cyan':'orange'}">${p.name}</span>
-HP:${p.hp} MP:${p.mp} ￥:${p.money}
-</div>`
-).join("");
+if (!me) return;
 
 
-hand.innerHTML = me?.hand.map((c, i) =>
-`<button ${state.players[state.turn].id!==socket.id?'disabled':''}
-onclick="socket.emit('play',${i})">
-<img src="cards/${c.type}.png"><br>${c.type}
-</button>`
-).join("");
+me.hand.forEach((c, i) => {
+const box = document.createElement("div");
+box.className = "cardBox";
 
 
-log.innerHTML = state.log.slice(-6).join("<br>");
+const img = document.createElement("img");
+img.src = `cards/${c.type}.png`;
+img.className = "card";
+img.onclick = () => socket.emit("play", i);
+
+
+const power = document.createElement("div");
+power.className = "cardPower";
+power.innerText = `攻${c.power}`;
+
+
+box.appendChild(img);
+box.appendChild(power);
+hand.appendChild(box);
+});
+
+
+document.getElementById("startBtn").disabled = state.players.length < 2;
 });
